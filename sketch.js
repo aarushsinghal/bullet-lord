dc64cf60a2cb6c5dd59e2ed1e155c104988b7203
aarushsinghal <66258983+aarushsinghal@users.noreply.github.com>
2020-06-30 18:12:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(1600,400);
   speed=random(223,321);
   weight=random(30,52);
-  thickness=random(28,83)
+  thickness=random(28,83);
   bullet = createSprite(100, 200, 50, 50);
   bullet.velocityX=speed;
   bullet.shapeColor="white";
@@ -19,7 +19,7 @@ function draw() {
   background(5,3,7);  
   if(wall.x-bullet.x<(bullet.width+wall.width)/2){
    bullet.velocityX=0;
-    damage=(0.5 * Weight * Speed * Speed)/thickness*thickness*thickness;
+    damage=(0.5 * weight * speed * speed)/(thickness*thickness*thickness);
   
 
   if(damage<10){
